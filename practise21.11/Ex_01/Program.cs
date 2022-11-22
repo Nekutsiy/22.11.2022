d901eb1c150a670264fba8f624cc0678f183c44c
Денис <1989nekutsiy@gmail.com>
2022-11-22 14:42:52 +0300

    1

---
+++ practise21.11/Ex_01/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит случайное
+// трехзначное число, и выводит цифру этого числа
+
+int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+int result = (num/100)*10 +num%10;
+Console.WriteLine(result);
+
